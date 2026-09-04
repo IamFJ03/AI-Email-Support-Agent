2bee8b6c5705ac64ec58208b786da006b7057082
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-09-04 19:50:50 +0530

    RAG Pipeline created

--- RagPipeline.py
+++ RagPipeline.py
@@ -1,5 +1,7 @@
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyMuPDFLoader, Docx2txtLoader, UnstructuredMarkdownLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -22,4 +24,39 @@ class RagRetriever:
 
         return len(result["ids"])
 
+    def file_loader(self, source):
     
+        if source.endswith(".pdf"):
+            loader = PyMuPDFLoader(source)
+    
+        elif source.endswith(".docx"):
+            loader = Docx2txtLoader(source)
+    
+        elif source.endswith(".md"):
+            loader = UnstructuredMarkdownLoader(source)
+    
+        elif source.endswith(".txt"):
+            loader = TextLoader(source)
+    
+        else:
+            raise ValueError("Unsupported source")
+            
+        self.document = loader.load()
+
+    def text_splitting(self):
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 500,
+            chunk_overlap = 100
+        )
+
+        self.chunks = text_splitter.split_documents(self.document)
+
+    def document_retriever(self, question):
+        retriever = self.vector_store.as_retriever(
+            search_kwargs = {
+                "k": 4
+            }
+        )
+
+        result = retriever.invoke(question)
+        return result
